195386e849cf9bfc66ae0a55f89e79a6eef4c5e0
Ajinkya_Ban <ajinkaban@gmail.com>
2024-03-09 12:06:12 +0530

    display data and update code

--- update.php
+++ update.php
@@ -1,5 +1,10 @@
 <?php
 include("connection.php");
+$id = $_GET['id'];
+$query = "select * from emp where id ='$id'";
+$data = mysqli_query($conn, $query);
+$total = mysqli_num_rows($data);
+$result = mysqli_fetch_assoc($data);
 ?>
 
 <!DOCTYPE html>
@@ -22,36 +27,36 @@ include("connection.php");
         </div>
 
         <div class="form-data">
-           <form action="index.php" method="post">
+           <form action="#" method="post">
             <table>
                 <tr>
                     <td> <label for="fname">First Name</label></td>
-                    <td><input type="text" name="fname" id="fname" autocomplete="off" required></td>
+                    <td><input type="text" name="fname" value="<?php echo $result['fname'] ?>" id="fname" autocomplete="off" required></td>
                 </tr>
                 <tr>
                     <td> <label for="lname">Last Name</label></td>
-                    <td><input type="text" name="lname" id="lname" autocomplete="off" required></td>
+                    <td><input type="text" name="lname" value="<?php echo $result['lname'] ?>" id="lname" autocomplete="off" required></td>
                 </tr>
                 <tr>
                     <td> <label for="email">Enter Email</label></td>
-                    <td><input type="email" name="email" id="email" autocomplete="off" required></td>
+                    <td><input type="email" name="email" value="<?php echo $result['email'] ?>" id="email" autocomplete="off" required></td>
                 </tr>
                 <tr>
                     <td> <label for="mob">Enter Mobile</label></td>
-                    <td><input type="text" name="mob" id="mob" autocomplete="off" required></td>
+                    <td><input type="text" name="mob" id="mob" value="<?php echo $result['mob'] ?>" autocomplete="off" required></td>
                 </tr>
                 <tr>
                     <td> <label for="pass">Enter Password</label></td>
-                    <td><input type="password" name="pass" id="pass" autocomplete="off" required></td>
+                    <td><input type="password" name="pass" value="<?php echo $result['pass'] ?>" id="pass" autocomplete="off" required></td>
                 </tr>
                 <tr>
                     <td> <label for="cpass">Enter Confirm Password</label></td>
-                    <td><input type="password" name="cpass" id="cpass" autocomplete="off" required></td>
+                    <td><input type="password" value="<?php echo $result['cpass'] ?>" name="cpass" id="cpass" autocomplete="off" required></td>
                 </tr>
 
                 <tr>
                     <td></td>
-                    <td><input type="submit" value="Register" name="register"></td>
+                    <td><input type="submit" value="Update Details" name="update"></td>
                 </tr>
             </table>
 
@@ -63,7 +68,7 @@ include("connection.php");
 
 <?php
 
-if($_POST['register'])
+if($_POST['update'])
 {
 
     $fname = $_POST['fname'];
@@ -73,16 +78,18 @@ if($_POST['register'])
     $pass = $_POST['pass'];
     $cpass = $_POST['cpass'];
     
-    $query = "insert into emp(fname, lname, email, mob, pass, cpass) values('$fname','$lname','$email','$mob','$pass','$cpass')";
+
+    $query = "update emp set fname='$fname',lname='$lname',email='$email',mob='$mob',pass='$pass',cpass='$cpass' where id = '$id'";
+
     $data = mysqli_query($conn,$query);
 
     if($data)
     {
-        echo "<script>alert('Data inserted')</script>";
+        echo "<script>alert('one record updated')</script>";
     }
     else
     {
-        echo "<script>alert('Data not inserted')</script>".mysqli_connect_error();
+        echo "<script>alert('Not found')</script>".mysqli_connect_error();
     }
 }
 
